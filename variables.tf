011e0809d0c48e73c34f0bcdbd6c513101b4225d
n01520500 <106194137+n01520500@users.noreply.github.com>
2023-03-15 16:56:03 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "tags" {
   type        = map(string)
-  description = "A mapping of tags to assign to the resource."
   default = {
     Project        = "Automation Project – Assignment 1"
     Name           = "kavya.sharma"
